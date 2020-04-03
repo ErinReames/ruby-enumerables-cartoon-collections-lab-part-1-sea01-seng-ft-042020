f55007939dac7d7ea7f8e1fe60b1c82fc6121651
Erin Reames <italiagirl83@gmail.com>
2020-04-03 23:18:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-   array.each_with_index do |array| 
+   array.each_with_index do |array, index| 
     puts "Hello #{array}!"
   end
 end
